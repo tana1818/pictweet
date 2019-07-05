1c0b9e65d9b9eac1e4d60bc53f177d4514818890
tana1818 <tanakagen1124@gmail.com>
2019-07-05 20:39:27 +0900

    add_to_tweet_function

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  get 'tweets' => 'tweets#index'
+  devise_for :users
+  root 'tweets#index'
+  resources :tweets do
+    resources :comments, only: [:create] 
+  end
+  resources :users, only: [:show]
 end
